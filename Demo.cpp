66069fcfa4a119cc887643825fe618b03f7a4c39
Tal <talzhtc@gmail.com>
2022-02-01 18:21:52 +0200

    finished arithmetic operator demos

--- Demo.cpp
+++ Demo.cpp
@@ -3,6 +3,9 @@
  *
  * @author Erel Segal-Halevi
  * @since 2019-02
+ * 
+ * Edited by Tal Zichlinsky
+ * @since 2022-02
  */
 
 #include <iostream>
@@ -11,31 +14,44 @@
 #include <stdexcept>
 using namespace std;
 
-#include "NumberWithUnits.hpp"
+#include "Matrix.hpp"
 using namespace ariel;
 
 int main() {
-  ifstream units_file{"units.txt"};
-  NumberWithUnits::read_units(units_file);
+  
+  double identity[3][3] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+  double arr[3][3] = {{3, 0, 0}, {0, 3, 0}, {0, 0, 3}};
+  Matrix a{identity};  // constructor taking a 2d array
+  cout << a << endl;
+  /* prints [1 0 0]
+            [0 1 0]
+            [0 0 1]*/
 
-  NumberWithUnits a{2, "km"};   // 2 kilometers
-  cout << a << endl;           // Prints "2[km]".
-  cout << (-a) << endl;    // Prints "-2[km]"
-  cout << (3*a) << endl;    // Prints "6[km]"
+  cout << (-a) << endl;
+  /* prints [-1 0 0]
+            [0 -1 0]
+            [0 0 -1]*/
+  cout << (3*a) << endl;
+  /* prints [-3 0 0]
+            [0 -3 0]
+            [0 0 -3]*/
 
-  NumberWithUnits b{300, "m"};  // 300 meters
-  cout << (a+b) << endl;   // Prints "2.3[km]". Note: units are determined by first number (a).
-  cout << (b-a) << endl;   // Prints "-1700[m]". Note: units are determined by first number (b).
+  Matrix b{arr};
+  cout << (a+b) << endl;  // prints the 0 matrix
+  cout << (b-a) << endl;
+  /* prints [6 0 0]
+            [0 6 0]
+            [0 0 6]*/
 
   cout << boolalpha; // print booleans as strings from now on:
   cout << (a>b) << endl;  // Prints "true"
   cout << (a<=b) << endl;  // Prints "false"
-  cout << (a==NumberWithUnits{2000, "m"}) << endl;  // Prints "true"
+  cout << (a==Matrix{2000, "m"}) << endl;  // Prints "true"
 
   istringstream sample_input{"700 [ kg ]"};
   sample_input >> a;
   cout << a << endl;   // Prints "700[kg]"
-  cout << (a += NumberWithUnits{1, "ton"}) << endl;  // prints "1700[kg]"
+  cout << (a += Matrix{1, "ton"}) << endl;  // prints "1700[kg]"
   cout << a << endl;   // Prints "1700[kg]". Note that a has changed.
 
   try {
